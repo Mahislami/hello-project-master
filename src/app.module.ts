3fa698a0760006d418ee964885fa95fd1c1b0276
Mahislami <mahdislami1377@gmail.com>
2020-12-15 19:25:15 +0000

    Adding Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import UserEntity from 'db/user.entity';
 import BooksModule from 'src/books/books.module';
 import GenreModule from 'src/genre/genre.module';
@@ -18,6 +20,10 @@ import GenreEntity from 'db/genre.entity';
     ),
 
     TypeOrmModule.forRoot(),
+
+    AuthModule,
+
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
